ae14874b6d52c39894f8e21c766fba3c31576ca5
mmoldenburg <muehleisen.michael@web.de>
2022-01-13 21:33:31 +0100

    adding nginx

--- my-app/tailwind.config.js
+++ my-app/tailwind.config.js
@@ -2,7 +2,12 @@ module.exports = {
   purge: ['./public/**/*.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'vue-grey': '#2c3e50',
+        'vue-green': '#42b983'
+        }        
+    },
   },
   variants: {
     extend: {},
