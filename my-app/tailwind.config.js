9622c3cd029af18644374ee133e34237356a600d
mmoldenburg <muehleisen.michael@web.de>
2022-01-13 21:09:10 +0100

    adding app-image

--- my-app/tailwind.config.js
+++ my-app/tailwind.config.js
@@ -1,13 +1,8 @@
 module.exports = {
-  purge: ['./public/**/*.html', './src/**/*.vue'],
+  purge: ['./public/**/*.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {
-      colors: {
-        'vue-grey': '#2c3e50',
-        'vue-green': '#42b983'
-      }
-    },
+    extend: {},
   },
   variants: {
     extend: {},
