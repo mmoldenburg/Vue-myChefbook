fbdcb9e05b3d4eed0f47d2832387e17da7e63c02
mmoldenburg <muehleisen.michael@web.de>
2022-01-18 19:05:55 +0100

    adding curser:pointer RecipeDetails.vue - delete-button

--- my-app/src/views/RecipeDetails.vue
+++ my-app/src/views/RecipeDetails.vue
@@ -8,7 +8,7 @@
                 <img :src="document.imageUrl" alt="recipe picture" class="rounded shadow-xl ">
             </div>
            
-            <div @click="handleClick" class=" lg:absolute lg:top-8 lg:right-12">
+            <div @click="handleClick" class="cursor-pointer  lg:absolute lg:top-8 lg:right-12">
                 <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6 mx-auto lg:w-12 lg:h-12 " fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
                 </svg>
